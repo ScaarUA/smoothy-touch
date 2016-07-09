9ad34525852825717642d094c74a95060e716d00
Bohdan Huseinov <bohdan_huseinov@epam.com>
2016-07-10 00:16:23 +0300

    Opacity, stack on edges

--- src/js/smoothy-touch.js
+++ src/js/smoothy-touch.js
@@ -49,7 +49,7 @@ if (!window.smoothyTouch) {
                 } else {
                     this.children[0].style.transition = 'none';
                 }
-                return this.children[0].style.transform = newVal;
+                return this.children[0].style.webkitTransform = newVal;
             };
 
             _elem.changeOpacity = function (val, isEnd) {
@@ -74,25 +74,54 @@ if (!window.smoothyTouch) {
                 return this.children[1].style.opacity = val;
             };
 
+            _elem._getCurrentPosition = function() {
+                var coords = this.children[0].style.transform.match(/-?\d(?!d)[\.\d]*/g);
+                coords = coords || [];
+
+                return {
+                    x: parseFloat(coords[0]),
+                    y: parseFloat(coords[1]),
+                    z: parseFloat(coords[2])
+                }
+            };
+
+            function _getMeasure() {
+                if(~allParams.width.indexOf('%')) {
+                    return screen.width * customWidth / 100;
+                }
+                else if(~allParams.width.indexOf('px')) {
+                    return customWidth;
+                }
+            }
+
+            //TODO: optimize for old browsers
             doc.addEventListener('touchstart', touchstart);
             doc.addEventListener('touchmove', touchmove);
+            doc.addEventListener('touchend', touchend);
+            doc.addEventListener('touchcancel', touchend);
 
             var startTouch = null,
-                startPos = 0,
-                curPos = 0,
-                curTouch = null,
-                touchId = null;
+                // startPos = 0,
+                curPos = -100,
+                newPos = 0,
+                inc = 0,
+                touchId = null,
+                customWidth = parseInt(allParams.width, 10),
+                measure = 0;
 
             function touchstart(evt) {
+                curPos = _elem._getCurrentPosition().x;
+                if(isNaN(curPos)) curPos = -100;
                 if(evt.touches.length !== 1 || touchId !== null) {
                     return;
                 }
-                if(evt.touches[0].clientX > 10) {
+                if(evt.touches[0].clientX > 15 && curPos === -100) {
                     return;
                 }
 
                 touchId = evt.touches[0].identifier;
                 startTouch = evt.touches[0].clientX;
+                measure = _getMeasure();
             }
 
             function touchmove(evt) {
@@ -102,17 +131,30 @@ if (!window.smoothyTouch) {
                         return;
                     }
                     var diff = evt.changedTouches[i].clientX - startTouch;
-                    if(allParams.width.indexOf('%')) {
-                        curPos = diff * 100 / (screen.width * parseInt(allParams.width, 10) / 100);
+
+                    inc = diff * 100 / measure;
+                    newPos = inc + curPos;
+                    console.log(inc, curPos);
+                    if(newPos > 0 || newPos < -100) {
+                        return;
                     }
-                    if(~allParams.width.indexOf('px')) {
-                        curPos = diff * 100 / parseInt(allParams.width, 10);
-                    }
-                    console.log(curPos);
-                    curPos = curPos - startPos - 100;
-                    _elem.changeTransform({posX: curPos + '%'});
-                    _elem.changeOpacity(curTouch * allParams.maxOpacity / 100);
+
+                    _elem.changeTransform({posX: newPos + '%'});
+                    _elem.changeOpacity(Math.abs(-100 - newPos) * allParams.maxOpacity / 100);
                 }
+            }
+            function touchend(evt) {
+                touchId = null;
+                // curPos = newPos;
+                if(newPos > -50) {
+                    newPos = 0;
+                } else {
+                    newPos = -100
+                }
+                _elem.changeTransform({posX: newPos + '%'}, true);
+                _elem.changeOpacity(Math.abs(-100 - newPos) * allParams.maxOpacity / 100, true);
+                curPos = newPos;
+
             }
         }
 
@@ -135,6 +177,6 @@ if (!window.smoothyTouch) {
         return getElement;
     })();
 } else {
-    console.log('SmoothyTouch already exist');
+    console.log('SmoothyTouch variable already exist');
     window.smoothyTouch = {};
 }
